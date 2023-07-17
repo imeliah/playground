0a7b9f4dff0903894c0a018bbf88ed3c85c6c345
張継慧 <im.zhangjihui@gmail.com>
2023-07-17 19:25:53 +0800

    查询参数

--- python/main.py
+++ python/main.py
@@ -1,14 +1,23 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+# 创建数据模型
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None  # 可选，默认值None
+    price: float
+    tax: Union[float, None] = None  # 可选，默认值None
+
 
 app = FastAPI()
 
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"},
-]
-
 
 @app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+async def read_items(q: Union[str, None] = None):
+    result = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        result.update({"q": q})
+    return result
