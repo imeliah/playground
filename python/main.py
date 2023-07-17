f7f41f8e8d5d32a28ea41e2e7b35959f6782ca2f
張継慧 <im.zhangjihui@gmail.com>
2023-07-17 21:12:10 +0800

    嵌入请求体参数

--- python/main.py
+++ python/main.py
@@ -1,23 +1,28 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 from pydantic import BaseModel
-
-
-# 创建数据模型
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None  # 可选，默认值None
-    price: float
-    tax: Union[float, None] = None  # 可选，默认值None
-
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(q: Union[str, None] = None):
-    result = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        result.update({"q": q})
-    return result
+class User(BaseModel):
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    creator: User
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int,
+                      item: Item = Body(embed=True),  # item键，值中包含模型内容的json
+                      ):
+    results = {"item_id": item_id, "item": item}
+    print(item)
+    print(item.creator)
+    return results
